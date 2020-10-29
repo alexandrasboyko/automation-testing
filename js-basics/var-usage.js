b2d44e6aa3f8f40371c07ea3f3f781fe77d9bd17
alexandrasboyko <alexandrasboyko@gmail.com>
2020-10-29 12:32:36 +0200

    7

--- js-basics/var-usage.js
+++ js-basics/var-usage.js
@@ -6,3 +6,13 @@ function sum (a,b){
     console.log ("sum=", a+b)
 }
 sum(Numbers.one, Numbers.two)
+
+const BB= {
+    a:1,
+    b:11,
+    c:10
+}
+function Sum (a,b,c){
+console.log("sum=", a+b+c)
+}
+Sum(BB.a,BB.b,BB.c)
