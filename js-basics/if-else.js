a64010628e04b208fa3148c53b825a0f31c58196
alexandrasboyko <alexandrasboyko@gmail.com>
2020-11-05 22:07:41 +0200

    15

--- js-basics/if-else.js
+++ js-basics/if-else.js
@@ -1,7 +1,7 @@
-let age = 14;
+let age = 98;
 
-if (!(14 <= age && age <=90)) {
-    console.log(age, "НЕ в диапазоне от 14 до 90");
+if (age < 14 || age > 90) {
+    console.log(age, " ne в диапазоне от 14 до 90");
 }
-else {console.log(age," в диапазоне")}
+else {console.log(age,"  в диапазоне ")}
 
