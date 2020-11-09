72cf0a31ab3c8c287cbc1989caee8c6c208b4d08
alexandrasboyko <alexandrasboyko@gmail.com>
2020-11-09 13:55:07 +0200

    16

--- js-basics/study.js
+++ js-basics/study.js
@@ -27,3 +27,32 @@
 // undefined
 // Infinity+-
 //NaN
+// x=12
+// console.log(typeof x)
+
+// 4  Приоритет Операторов
+// currentYear=2020
+// bornYear=1996
+// fullaAge=24
+// const myAge=(currentYear-bornYear)<=fullaAge
+// console.log(myAge)
+
+// 5  Условные операторы
+// const myday="Monday"
+//
+// const num1=5
+// const num2="5"
+// console.log(num1==num2) //--->True
+// console.log(num1===num2)//--->False
+
+//const day=true
+//if (day===true) //аналогично запись if (day){}//
+// {
+//     console.log("myday");
+// }
+// else {
+//     console.log("not my day")}
+
+  //Тернарка
+ // аналогичная запись day===true//
+ // day? console.log("myday"): console.log("not my day")  
